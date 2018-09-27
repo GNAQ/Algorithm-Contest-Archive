00a0dfa7f4893f319b4761ac65d62b80a8b3394d
GNAQ <no1061234176@outlook.com>
2018-09-27 22:04:16 +0800

    Oh,god,mobius is so geniusgit add -A

---
+++ qwq.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<iterator>
+#include<cstdlib>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#define ll long long
+using namespace std;
+
+
+
+void readx(int& x)
+{
+	x=0; int k=1; char ch=0;
+	while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
+	while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
+	x*=k;
+}
+
+
+
+int main()
+{
+	
+	
+	
+}
