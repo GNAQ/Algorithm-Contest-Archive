32b21b4b9146180b3de900933a4f217570eb36b0
GNAQ <no1061234176@outlook.com>
2018-10-11 18:42:02 +0800

    Daily

--- qwq.cpp
+++ qwq.cpp
@@ -13,7 +13,32 @@
 #define ll long long
 using namespace std;
 
+int n,mapx[11][11];
+int dp[11][11][11][11];
+
+template<typename int_t>
+void readx(int_t& x)
+{
+	x=0; int_t k=1; char ch=0;
+	while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
+	while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
+	x*=k;
+}
+
 int main()
 {
+	readx(n); int txp,typ;
+	for (int i=1;i<=n;i++)
+		for (int j=1;j<=n;j++)
+		{
+			readx(txp); readx(typ);
+			readx(mapx[txp][typ]);
+		}
+	
+	for (int i=1;i<=n;i++)
+		for (int j=1;j<=n;j++)
+			for (int k=1;k<=)
+	
+	
 	
 }
