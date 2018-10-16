ccb769472aff3c28df81dd8116b60d55a52a87b2
GNAQ <no1061234176@outlook.com>
2018-10-16 21:09:32 +0800

    WTF of this problem!!!!!!

--- qwq.cpp
+++ qwq.cpp
@@ -12,13 +12,15 @@
 using namespace std;
 
 int N,M,Q,s_x,s_y,t_x,t_y,e_x,e_y;
-bool mapx[100][100];
-const int wayx[4]={0,-1,0,1},wayy[4]={-1,0,1,0};
+bool mapx[50][50];
+const int dx[4]={0,-1,0,1},dy[4]={-1,0,1,0};
 
-int dist[100][100];
-bool vis[100][100];
-queue<pair<int,int>> que;
-
+int dist[50][50];
+queue<pair<int,int>> qu;
+queue<int> que;
+bool vis[30010];
+int cod[50][50][4],dis[30010];
+vector<pair<int,int>> edge[30010];
 
 //Funcs
 template<typename int_t>
@@ -29,121 +31,86 @@ void readx(int_t& x)
 	while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
 	x*=k;
 }
+void Is(int fx,int tx,int wx) { edge[fx].push_back({tx,wx}); }
 
-bool Ok(int x,int y)
+bool Get_Dist(int sx,int sy,int ex,int ey,int d)
 {
-	if (x<=0 || y<=0 || x>N || y>M) return false;
-	return true;
-}
-
-bool Get_Dist(int sx,int sy)
-{
-	pair<int,int> cac1,cac2;
-	if (!Ok(sx,sy) || !mapx[sx][sy]) return false;
-	memset(dist,0x3f,sizeof dist);
-	for (int i=1;i<=N;i++) for (int j=1;j<=M;j++) vis[i][j]=!mapx[i][j];
+	pair<int,int> c1,c2;
+	memset(dist,-1,sizeof dist);
 	
-	que.push({sx,sy}); vis[sx][sy]=1; dist[sx][sy]=0;
-	while (!que.empty())
+	qu.push({ex,ey}); dist[ex][ey]=0; dist[sx][sy]=1;
+	while (!qu.empty())
 	{
-		cac1=que.front(); que.pop();
-		
+		c1=qu.front(); qu.pop();
 		for (int w=0;w<=3;w++) 
 		{
-			cac2.fst=cac1.fst+wayx[w]; cac2.snd=cac1.snd+wayy[w];
-			if (Ok(cac2.fst,cac2.snd) && !vis[cac2.fst][cac2.snd])
+			c2.fst=c1.fst+dx[w]; c2.snd=c1.snd+dy[w];
+			if (mapx[c2.fst][c2.snd] && dist[c2.fst][c2.snd]==-1)
 			{
-				que.push(cac2);
-				vis[cac2.fst][cac2.snd]=1;
-				dist[cac2.fst][cac2.snd]=dist[cac1.fst][cac1.snd]+1;
+				qu.push(c2);
+				dist[c2.fst][c2.snd]=dist[c1.fst][c1.snd]+1;
 			}
 		}
 	}
-	return true;
+	if (d==-1) return;
+	
+	int tmpx,tmpy;
+	for (int i=0;i<=3;i++)
+	{
+		tmpx=sx+dx[i]; tmpy=sy+dy[i];
+		if (!mapx[tmpx][tmpy] || dist[tmpx][tmpy]<=0) continue;
+		Is(cod[sx][sy][d],cod[sx][sy][i],dist[tmpx][tmpy]);
+	}
+	Is(cod[sx][sy][d],cod[ex][ey][(d+2)%4],1);
 }
 
-namespace SSSP
+void Build_Graph()
 {
-	queue<int> que;
-	bool vis[100010];
-	int cod[100][100][4],dis[100010];
+	int tmp_code=0;
+	for (int i=1;i<=N;i++) for (int j=1;j<=M;j++) for (int k=0;k<=3;k++) cod[i][j][k]=++tmp_code;
 	
-	vector<pair<int,int>> edge[100010];
-	void Is(int fx,int tx,int wx)
+	for (int i=1;i<=N;i++)
+		for (int j=1;j<=M;j++) if (mapx[i][j])
+			for (int w=0;w<=3;w++) if (mapx[i+dx[w]][j+dy[w]])
+				Get_Dist(i,j,i+dx[w],j+dy[w],w);
+}
+
+void SPFA()
+{
+	int cac=0;
+	memset(vis,0,sizeof(vis));
+	memset(dis,0x3f,sizeof(dis));
+	for (int i=0;i<=3;i++) if (mapx[s_x+dx[i]][s_y+dy[i]])
 	{
-		edge[fx].push_back({tx,wx});
-		edge[tx].push_back({fx,wx});
+		dis[cod[s_x][s_y][i]]=dist[s_x+dx[i]][s_y+dy[i]];
+		que.push(cod[s_x][s_y][i]);
 	}
-	void Build_Graph()
+	while (!(que.empty()))
 	{
-		int x_1,x_2,y_1,y_2;
-		for (int i=1;i<=N;i++)
-			for (int j=1;j<=M;j++) if (mapx[i][j])
-				for (int k=0;k<=3;k++)
-				{
-					for (int l=k+1;l<=3;l++)
-					{
-						x_1=i+wayx[k]; y_1=j+wayy[k]; x_2=i+wayx[l]; y_2=j+wayy[l];
-						if (Ok(x_1,y_1) && Ok(x_2,y_2))
-						{
-							mapx[i][j]=0;
-							if (Get_Dist(x_1,y_1) && dist[x_2][y_2]<=N+M+2) 
-							{
-							// 	cout<<"Is "<<i<<" "<<j<<" "<<k<<" "<<l<<" "<<dist[x_2][y_2]<<endl;
-								Is(cod[i][j][k],cod[i][j][l],dist[x_2][y_2]);
-							}
-							mapx[i][j]=1;
-						}
-					}
-				}
-		for (int i=1;i<=N;i++)
-			for (int j=1;j<=M;j++) if (mapx[i][j])
-				for (int k=0;k<=3;k++)
-				{
-					x_1=i+wayx[k]; y_1=j+wayy[k];
-					if (Ok(x_1,y_1) && mapx[x_1][y_1]) 
-					{
-						// cout<<"Is "<<i<<" "<<j<<" "<<x_1<<" "<<y_1<<endl;
-						Is(cod[i][j][k],cod[x_1][y_1][(k+2)%4],1);
-					}
-				}
-	}
-	
-	void SPFA(int source)
-	{
-		int cac=0;
-		memset(SSSP::dis,0x3f,sizeof(SSSP::dis)); SSSP::dis[source]=0;
-		SSSP::que.push(source);
-		while (!(SSSP::que.empty()))
-		{
-			cac=SSSP::que.front(); SSSP::que.pop();
-			for (auto v:(SSSP::edge[cac]))
+		cac=que.front(); que.pop();
+		for (auto v:(edge[cac]))
+			if (dis[v.fst]>dis[cac]+v.snd)
 			{
-				if (SSSP::dis[v.fst]>SSSP::dis[cac]+v.snd)
+				dis[v.fst]=dis[cac]+v.snd;
+				if (!vis[v.fst])
 				{
-					SSSP::dis[v.fst]=SSSP::dis[cac]+v.snd;
-					if (!SSSP::vis[v.fst])
-					{
-						SSSP::vis[v.fst]=1;
-						SSSP::que.push(v.fst);
-					}
+					vis[v.fst]=1;
+					que.push(v.fst);
 				}
 			}
-		}
 	}
-};
+}
+
 
 int main()
 {
+	freopen("dat.in","r",stdin);
+	freopen("dat.out","w",stdout);
+	
 	readx(N); readx(M); readx(Q);
 	for (int i=1;i<=N;i++)
 		for (int j=1;j<=M;j++) readx(mapx[i][j]);
-	
-	int tmp_code=0;
-	for (int i=1;i<=N;i++)
-		for (int j=1;j<=M;j++)
-			for (int k=0;k<=3;k++) SSSP::cod[i][j][k]=++tmp_code;
-	SSSP::Build_Graph();
+	Build_Graph();
 	
 	int tmpx,tmpy;
 	while (Q--)
@@ -151,34 +118,14 @@ int main()
 		readx(e_x); readx(e_y);
 		readx(s_x); readx(s_y); readx(t_x); readx(t_y);
 		
-		if (!mapx[e_x][e_y] || !mapx[s_x][s_y] || !mapx[t_x][t_y])
-		{
-			printf("-1\n"); 
-			continue;
-		}
+		if (!mapx[e_x][e_y] || !mapx[s_x][s_y] || !mapx[t_x][t_y]) { printf("-1\n");  continue; }
+		if (s_x==t_x && s_y==t_y) { printf("0\n"); continue; }
 		
-		mapx[s_x][s_y]=0;
-		Get_Dist(e_x,e_y);
-		mapx[s_x][s_y]=1;
-		// cout<<Get_Dist(e_x,e_y)<<endl;
-		// for (int i=1;i<=N;i++) for (int j=1;j<=M;j++) printf("%d%c",dist[i][j]," \n"[j==M]);
+		int ans=2*1e9;
 		
-		int ans=2*1e9,tans;
-		for (int i=0;i<=3;i++) 
-		{
-			tmpx=s_x+wayx[i]; tmpy=s_y+wayy[i];
-			if (!Ok(tmpx,tmpy) || !mapx[tmpx][tmpy]) continue;
-			SSSP::SPFA(SSSP::cod[s_x][s_y][i]);
-			
-			tans=2*1e9;
-			for (int j=0;j<=3;j++) 
-				tans=min(tans,dist[tmpx][tmpy]+SSSP::dis[SSSP::cod[t_x][t_y][j]]);
-			ans=min(ans,tans);
-		}
 		
-		if (ans>=0x3f3f3f3f) printf("-1\n");
-		else printf("%d\n",ans);
+		
 	}
-	system("pause");
+	// system("pause");
 	return 0;
 }
