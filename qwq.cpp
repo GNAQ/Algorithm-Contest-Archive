50c7c2a4c92f23fbf04d0a2281929dd88bdd8a4b
GNAQ <no1061234176@outlook.com>
2018-10-19 07:56:45 +0800

    Morning Prac ready to start

---
+++ qwq.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<cmath>
+#include<algorithm>
+#include<iterator>
+#include<cstdlib>
+#include<queue>
+#include<vector>
+#include<map>
+#include<set>
+#define ll long long
+using namespace std;
+
+
+
+template<typename int_t>
+void readx(int_t& x)
+{
+	x=0; int_t k=1; char ch=0;
+	while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
+	while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
+	x*=k;
+}
+
+
+
+int main()
+{
+	
+	
+	
+}
