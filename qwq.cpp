e112770d780f6ec9256ab68203349123de35e020
GNAQ <no1061234176@outlook.com>
2018-10-19 20:47:42 +0800

    Evening prac

--- qwq.cpp
+++ qwq.cpp
@@ -14,15 +14,18 @@
 using namespace std;
 
 int n,m;
-vector<int> edge[100010];
-int W[100010];
-struct Paths
-{
-	int u,v,lca;
-}pth[100010];
+vector<int> edge[300010];
+int W[300010];
+struct Paths { int u,v,lca,dist; }pth[300010];
+int dep[300010],l[300010][22];
 
-//LCA
-int dep[100010],l[100010][22];
+//paths
+int st[300010];
+vector<int> ps[300010],invs[300010],pn[300010];
+
+//bucket
+int buc[600010];
+int ans[300010];
 
 template<typename int_t>
 void readx(int_t& x)
@@ -55,9 +58,28 @@ int LCA(int u,int v)
 	return l[u][0];
 }
 
+void DFS1(int now,int fa)
+{
+	int tmp=buc[dep[now]+W[now]];
+	for (auto v:edge[now]) if (v!=fa) DFS1(v,now);
+	buc[dep[now]]+=st[now];
+	ans[now]+=buc[dep[now]+W[now]]-tmp;
+	for (auto v:ps[now]) buc[dep[v]]--;
+}
+
+void DFS2(int now,int fa)
+{
+	int tmp=buc[W[now]-dep[now]+n];
+	for (auto v:edge[now]) if (v!=fa) DFS2(v,now);
+	for (auto v:invs[now]) buc[v+n]++;
+	ans[now]+=buc[W[now]-dep[now]+n]-tmp;
+	for (auto v:pn[now]) buc[v+n]--;
+}
 
 int main()
 {
+	// freopen("dat.in","r",stdin);
+	// freopen("dat.out","w",stdout);
 	readx(n); readx(m); int fx,tx;
 	for (int i=1;i<n;i++) 
 	{
@@ -70,10 +92,18 @@ int main()
 	{
 		readx(pth[i].u); readx(pth[i].v);
 		pth[i].lca=LCA(pth[i].u,pth[i].v);
+		pth[i].dist=dep[pth[i].u]+dep[pth[i].v]-2*dep[pth[i].lca];
+		
+		st[pth[i].u]++;
+		ps[pth[i].lca].push_back(pth[i].u);
+		invs[pth[i].v].push_back(pth[i].dist-dep[pth[i].v]);
+		pn[pth[i].lca].push_back(pth[i].dist-dep[pth[i].v]);
 	}
 	
-	
-	
-	
-	
+	DFS1(1,0);
+	DFS2(1,0);
+	for (int i=1;i<=m;i++) 
+		if (dep[pth[i].u]==dep[pth[i].lca]+W[pth[i].lca]) ans[pth[i].lca]--;
+	for (int i=1;i<=n;i++) printf("%d%c",ans[i]," \n"[i==n]);
+	return 0;
 }
