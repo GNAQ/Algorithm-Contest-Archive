38e196b51aef590bfaccce38439c3681dc543937
GNAQ <no1061234176@outlook.com>
2018-10-19 22:05:58 +0800

    Night upd

--- qwq.cpp
+++ qwq.cpp
@@ -3,29 +3,20 @@
 #include<cstring>
 #include<string>
 #include<cmath>
-#include<iterator>
 #include<algorithm>
+#include<iterator>
 #include<cstdlib>
 #include<queue>
 #include<vector>
-#include<map>
 #include<set>
+#include<map>
 #define ll long long
 using namespace std;
 
-int n,m;
-vector<int> edge[300010];
-int W[300010];
-struct Paths { int u,v,lca,dist; }pth[300010];
-int dep[300010],l[300010][22];
+int n,G[20][20],m;
 
-//paths
-int st[300010];
-vector<int> ps[300010],invs[300010],pn[300010];
-
-//bucket
-int buc[600010];
-int ans[300010];
+int dp[10010][20][20];
+bool can[20];
 
 template<typename int_t>
 void readx(int_t& x)
@@ -35,75 +26,64 @@ void readx(int_t& x)
 	while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
 	x*=k;
 }
-void Is(int fx,int tx)
+
+void Floyd()
 {
-	edge[fx].push_back(tx);
-	edge[tx].push_back(fx);
-}
-void LCADFS(int now,int ndep,int fa)
-{
-	dep[now]=ndep; l[now][0]=fa;
-	for (int i=1;i<=20;i++) l[now][i]=l[l[now][i-1]][i-1];
-	
-	for (auto v:edge[now]) if (v!=fa)
-		LCADFS(v,ndep+1,now);
-}
-int LCA(int u,int v)
-{
-	if (dep[u]<dep[v]) swap(u,v);
-	for (int i=20;i>=0;i--) if (dep[l[u][i]]>=dep[v]) u=l[u][i];
-	if (u==v) return v;
-	for (int i=20;i>=0;i--) if (l[u][i]!=l[v][i])
-		{ u=l[u][i]; v=l[v][i]; }
-	return l[u][0];
+	for (int k=1;k<=n;k++)
+		for (int i=1;i<=n;i++)
+			for (int j=1;j<=n;j++) G[i][j]=min(G[i][j],G[i][k]+G[k][j]);
 }
 
-void DFS1(int now,int fa)
+void Decod(int val)
 {
-	int tmp=buc[dep[now]+W[now]];
-	for (auto v:edge[now]) if (v!=fa) DFS1(v,now);
-	buc[dep[now]]+=st[now];
-	ans[now]+=buc[dep[now]+W[now]]-tmp;
-	for (auto v:ps[now]) buc[dep[v]]--;
+	for (int i=1;i<=n;i++) can[i]=0;
+	for (int i=1;i<=n;i++) can[i]=val&(1<<(i-1));
 }
 
-void DFS2(int now,int fa)
-{
-	int tmp=buc[W[now]-dep[now]+n];
-	for (auto v:edge[now]) if (v!=fa) DFS2(v,now);
-	for (auto v:invs[now]) buc[v+n]++;
-	ans[now]+=buc[W[now]-dep[now]+n]-tmp;
-	for (auto v:pn[now]) buc[v+n]--;
-}
 
 int main()
 {
-	// freopen("dat.in","r",stdin);
-	// freopen("dat.out","w",stdout);
-	readx(n); readx(m); int fx,tx;
-	for (int i=1;i<n;i++) 
+	readx(n); readx(m); int fx,tx,wx;
+	memset(G,0x3f,sizeof G); for (int i=1;i<=n;i++) G[i][i]=0;
+	for (int i=1;i<=m;i++) 
 	{
-		readx(fx); readx(tx);
-		Is(fx,tx);
+		readx(fx); readx(tx); readx(wx);
+		if (G[fx][tx]>wx) G[fx][tx]=G[tx][fx]=wx;
 	}
-	LCADFS(1,1,0);
-	for (int i=1;i<=n;i++) readx(W[i]);
-	for (int i=1;i<=m;i++)
+	// Floyd();
+	
+	int lim=(1<<n); int onum;
+	memset(dp,0x3f,sizeof dp); dp[0][0][0]=0;
+	for (int sta=1;sta<lim;sta++)
 	{
-		readx(pth[i].u); readx(pth[i].v);
-		pth[i].lca=LCA(pth[i].u,pth[i].v);
-		pth[i].dist=dep[pth[i].u]+dep[pth[i].v]-2*dep[pth[i].lca];
+		Decod(sta); onum=0;
+		for (int i=1;i<=n;i++) onum+=can[i];
+		if (onum==1)
+		{
+			for (int i=1;i<=n;i++) if (can[i])
+			{
+				dp[sta][i][1]=0;
+				break;
+			}
+			continue;
+		} 
 		
-		st[pth[i].u]++;
-		ps[pth[i].lca].push_back(pth[i].u);
-		invs[pth[i].v].push_back(pth[i].dist-dep[pth[i].v]);
-		pn[pth[i].lca].push_back(pth[i].dist-dep[pth[i].v]);
+		for (int i=1;i<=n;i++) if (can[i])
+		{
+			for (int w=2;w<=onum;w++)
+			{
+				for (int j=1;j<=n;j++) if (can[j] && j!=i)
+				{
+					int pre=sta^(1<<(j-1));
+					dp[sta][i][w]=min(dp[sta][i][w],dp[pre][j][w-1]+G[j][i]*w);
+				}
+			}
+		}
 	}
 	
-	DFS1(1,0);
-	DFS2(1,0);
-	for (int i=1;i<=m;i++) 
-		if (dep[pth[i].u]==dep[pth[i].lca]+W[pth[i].lca]) ans[pth[i].lca]--;
-	for (int i=1;i<=n;i++) printf("%d%c",ans[i]," \n"[i==n]);
+	int ans=2*1e9;
+	for (int i=1;i<=n;i++)
+		for (int j=2;j<=n;j++) ans=min(ans,dp[lim-1][i][j]);
+	printf("%d\n",ans);
 	return 0;
 }
