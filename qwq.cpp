5cdc4fc84117b668c556600b6a80bbd38b6931b8
GNAQ <no1061234176@outlook.com>
2018-12-09 23:01:49 +0800

    Dayli

--- qwq.cpp
+++ qwq.cpp
@@ -8,56 +8,27 @@
 #include<cstdlib>
 #include<queue>
 #include<vector>
-#include<set>
 #include<map>
+#include<set>
 #define ll long long
 using namespace std;
 
-vector<pair<int, int>> edge[100010];
-int n, m;
 
-int tsiz[100010], hson[100010];
 
 template<typename int_t>
 void readx(int_t& x)
 {
-	x = 0; int_t k = 1; char ch = 0;
-	while (ch < '0' || ch > '9') { ch = getchar(); if (ch == '-') k = -1; }
-	while (ch >= '0' && ch <= '9') { x = x * 10 + ch - '0'; ch = getchar(); }
-	x *= k;
-}
-
-void Is(int fx, int tx, int wx)
-{
-	edge[fx].push_back({tx, wx});
-	edge[tx].push_back({fx, wx});
+    x=0; int_t k=1; char ch=0;
+    while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
+    while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
+    x*=k;
 }
 
 
-void GetHr(int now)
-{
-	
-	
-	
-}
 
-void Divs(int now)
-{
-	
-}
 int main()
 {
-	readx(n); int fx,tx,wx;
-	
-	for (int i = 1; i < n; i++) 
-	{
-		readx(fx); readx(tx); readx(wx);
-		Is(fx,tx,wx);
-	}
-	
-	for (int i=1; i<=n; i++)
-	{
-		
-	}
-	
+    
+    
+    
 }
