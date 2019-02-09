c21d7d129c52dc9eed74af249a4aeda1567de6f2
GNAQ <no1061234176@outlook.com>
2019-02-09 17:24:39 +0800

    daily updates

--- qwq.cpp
+++ qwq.cpp
@@ -1,21 +1,47 @@
 #include<cstdio>
 #include<iostream>
 #include<cstring>
+#include<string>
 #include<cmath>
 #include<algorithm>
+#include<iterator>
 #include<cstdlib>
+#include<vector>
+#include<queue>
+#include<map>
+#include<set>
+#define fst first
+#define snd second
 typedef long long ll;
 using namespace std;
 
-int n,q; char opt;
-struct ed
+int n;
+struct Point
 {
-	int pre,to;
-}edge[200010];
-int at=1,ptr[100010];
+	int x,y,z,id,w;
+	
+	Point(int inx=0,int iny=0,int inz=0,int inid=0)
+	{
+		x=inx; y=iny;
+		z=inz; id=inid;
+	}
+	
+	friend bool operator < (const Point A,const Point B) 
+	{
+		if (A.x==B.x)
+		{
+			if (A.y==B.y) return A.z<B.z;
+			return A.y<B.y;
+		}
+		return A.x<B.x;
+	}
+};
+Point pnt[200010];
+int ans[200010];
+map<Point,bool> mapx;
 
-template<typename int_inp>
-void readx(int_inp& x)
+template<typename inp_typ>
+void readx(inp_typ& x)
 {
 	x=0; int k=1; char ch=0;
 	while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
@@ -23,137 +49,78 @@ void readx(int_inp& x)
 	x*=k;
 }
 
-void Is(int fx,int tx)
+namespace FWT
 {
-	at++;
-	edge[at].pre=ptr[fx];
-	edge[at].to=tx;
-	ptr[fx]=at;
-	at++;
-	edge[at].pre=ptr[tx];
-	edge[at].to=fx;
-	ptr[tx]=at;
-}
-
-namespace HLD
-{
-	// Heavy-Ligit chain decomposition
-	#define LCH (inx<<1)
-	#define RCH (inx<<1|1)
+	int lim,tree[200010];
 	
-	struct Seg_Tree
+	void Upd(int pos,int val)
 	{
-		int l,r,mid,pos;
-	}tree[400010];
-	int _pos,_val,lx,rx;
-	
-	void BuildTree(int inx,int lxx,int rxx)
-	{
-		tree[inx].l=lxx; tree[inx].r=rxx;
-		tree[inx].mid=(lxx+rxx)>>1; tree[inx].pos=2*1e9;
-		if (lxx==rxx) return;
-		BuildTree(LCH,lxx,tree[inx].mid);
-		BuildTree(RCH,tree[inx].mid+1,rxx);
+		for (;pos<=lim;pos+=(pos&(-pos)))
+			tree[pos]+=val;
 	}
 	
-	void Upd(int inx)
+	int Qry(int pos)
 	{
-		if (tree[inx].l==tree[inx].r)
-		{
-			tree[inx].pos=tree[inx].l;
-			return;
-		}
-		if (_pos<=tree[inx].mid) Upd(LCH);
-		else Upd(RCH);
-		if (tree[RCH].pos<2*1e9) tree[inx].pos=tree[RCH].pos;
-		else tree[inx].pos=tree[LCH].pos;
-	}
-
-	int Qry(int inx)
-	{
-		if (tree[inx].l>=lx && tree[inx].r<=rx) return tree[inx].pos;
-		int ret=2*1e9;
-		if (rx>tree[inx].mid) 
-			if ((ret=Qry(RCH))!=2*1e9) return ret;
-		if (lx<=tree[inx].mid) ret=Qry(LCH);
+		int ret=0;
+		for (;pos;pos-=(pos&(-pos)))
+			ret+=tree[pos];
 		return ret;
-	}
-	
-	int dep[100010],anc[100010],hson[100010],tsiz[100010];
-	int dfn[100010],tst,topx[100010],id[100010];
-	
-	void Fancy(int now,int ndep,int fa)
-	{
-		dep[now]=ndep; anc[now]=fa; tsiz[now]=1; 
-		for (int v=ptr[now];v;v=edge[v].pre) if (edge[v].to!=fa)
-		{
-			Fancy(edge[v].to,ndep+1,now);
-			tsiz[now]+=tsiz[edge[v].to];
-			if (tsiz[hson[now]]<tsiz[edge[v].to]) hson[now]=edge[v].to;
-		}
-	}
-	
-	void Dreams(int now,int src)
-	{
-		dfn[now]=++tst; topx[now]=src; id[tst]=now;
-		if (hson[now]) Dreams(hson[now],src);
-		for (int v=ptr[now];v;v=edge[v].pre)
-			if (edge[v].to!=anc[now] && edge[v].to!=hson[now]) 
-				Dreams(edge[v].to,edge[v].to);
-	}
-	
-	#undef LCH
-	#undef RCH
-	
-	int Qry_Pnt(int u)
-	{
-		while (1)
-		{
-			lx=dfn[topx[u]]; rx=dfn[u];
-			int ans=Qry(1);
-			if (ans!=2*1e9) return id[ans];
-			u=anc[topx[u]];
-		}
-		return 1;
-	}
-	
-	void Mod_Pnt(int u)
-	{
-		_pos=dfn[u]; 
-		Upd(1);
 	}
 };
 
+bool cmp_x(const Point& a,const Point& b)
+{
+	return a.x<b.x;
+}
+
+void Solve(int l,int r)
+{
+	if (l==r) return;
+	int mid=(l+r)>>1;
+	
+	Solve(l,mid);
+	
+	cout<<"!!Solve "<<l<<" "<<r<<endl;
+	
+	int lb=l;
+	for (int rb=mid+1;rb<=r;rb++)
+	{
+		while (lb<=mid && pnt[lb].y<=pnt[rb].y) 
+		{
+			FWT::Upd(pnt[lb].z,pnt[lb].w);
+			lb++;
+		}
+		ans[pnt[rb].id]+=FWT::Qry(pnt[rb].z);
+	}
+	for (int i=l;i<=mid;i++) FWT::Upd(pnt[i].z,-1*pnt[i].w);
+	
+	
+	Solve(mid+1,r);
+}
+
 int main()
 {
-	freopen("tree1.in","r",stdin);
-	freopen("dat.out","w",stdout);
-	
-	readx(n); readx(q); int fx,tx;
-	for (int i=1;i<n;i++)
+	int tx,ty,tz,tmp_n,tmp_pos;
+	readx(tmp_n); readx(FWT::lim);
+	for (int i=1;i<=tmp_n;i++)
 	{
-		readx(fx); readx(tx);
-		Is(fx,tx);
-	}
-	
-	HLD::Fancy(1,1,0); HLD::Dreams(1,1);
-	HLD::BuildTree(1,1,n);
-	HLD::Mod_Pnt(1);
-	
-	// for (int i=1;i<=n;i++) printf("%d%c",HLD::dfn[i]," \n"[i==n]);
-	
-	for (int i=1;i<=q;i++)
-	{
-		opt=0; while (opt<'A' || opt>'Z') opt=getchar();
-		readx(fx);
-		if (opt=='C')
-		{
-			HLD::Mod_Pnt(fx);
-		}
+		readx(tx); readx(ty); readx(tz);
+		if ((tmp_pos=mapx[Point(tx,ty,tz)])!=0)
+			pnt[tmp_pos].w++;
 		else
 		{
-			printf("%d\n",HLD::Qry_Pnt(fx)); 
+			n++;
+			pnt[n]=Point(tx,ty,tz,n); pnt[n].w=1;
+			mapx[pnt[n]]=n;
 		}
 	}
+	mapx.clear();
+	sort(pnt+1,pnt+n+1,cmp_x);
 	
+	for (int i=1;i<=n;i++)
+		printf("%d %d %d %d\n",pnt[i].x,pnt[i].y,pnt[i].z,pnt[i].w);
+	
+	Solve(1,n);
+	
+	for (int i=1;i<=n;i++) printf("%d\n",ans[i]);
 }
