5d1803ace29ac5047a24741a4a5f20338fb3ca0f
GNAQ <no1061234176@outlook.com>
2019-05-02 22:44:42 +0800

    Temp LCT Upd

--- Template/树/动态树/LCT.cpp
+++ Template/树/动态树/LCT.cpp
@@ -3,124 +3,158 @@
 #include<cstring>
 #include<string>
 #include<cmath>
-#include<cstdlib>
 #include<algorithm>
 #include<iterator>
+#include<cstdlib>
 #include<queue>
 #include<vector>
-#define LCH tree[pos].ch[0]
-#define RCH tree[pos].ch[1]
+#include<queue>
+#include<vector>
 using namespace std;
+typedef long long ll;
 
-struct splay_tree { int fa,ch[2],val,rev; } tree[300010]={0};
-int troo,tsiz=0,n,m;
-int opt,cachex,cachey;
-int seq[300010]={0};
+int n,m,nodev[300010];
 
-inline void readx(int& x)
+template<typename inp_t>
+void readx(inp_t& x)
 {
-    x=0; int k=1; register char ch=0;
-    while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
-    while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
-    x*=k;
-}
-
-inline void updata(int pos) { tree[pos].val=tree[LCH].val^tree[RCH].val^seq[pos]; }
-inline void PutRev(int pos)
-{
-    swap(LCH,RCH);
-    tree[LCH].rev^=1; 
-	tree[RCH].rev^=1;
-}
-inline void pushdown(int pos)
-{
-    if (!tree[pos].rev) return;
-    if (LCH) tree[LCH].rev^=1; 
-    if (RCH) tree[RCH].rev^=1;
-    swap(LCH,RCH); tree[pos].rev^=1;
-}
-inline bool NaNroot(int pos) { return (tree[tree[pos].fa].ch[0]==pos || tree[tree[pos].fa].ch[1]==pos); }
-inline void Push_All(int pos)
-{
-    if (NaNroot(pos)) Push_All(tree[pos].fa);
-    pushdown(pos);
+	x=0; int k=1; char ch=0;
+	while (ch<'0' || ch>'9') { ch=getchar(); if (ch=='-') k=-1; }
+	while (ch>='0' && ch<='9') { x=x*10+ch-'0'; ch=getchar(); }
+	x*=k;
 }
 
-inline void rotatex(int pos)
+namespace LCT
 {
-    int f=tree[pos].fa,g=tree[f].fa,flag=(tree[f].ch[1]==pos);
-    
-    if (NaNroot(f)) tree[g].ch[(tree[g].ch[1]==f)]=pos;
-    if (tree[pos].ch[!flag]) tree[tree[pos].ch[!flag]].fa=f;
-    tree[f].ch[flag]=tree[pos].ch[!flag]; tree[f].fa=pos;
-    tree[pos].ch[!flag]=f; tree[pos].fa=g;
-    
-    updata(f); updata(pos);
-}
-inline void splay(int pos)
-{
-    Push_All(pos);
-    for (;NaNroot(pos);rotatex(pos))
-    {
-        if (NaNroot(tree[pos].fa))
-        {
-            int g=tree[tree[pos].fa].fa;
-            if ((tree[g].ch[1]==tree[pos].fa)==(tree[tree[pos].fa].ch[1]==pos)) rotatex(tree[pos].fa);
-            else rotatex(pos);
-        }
-    }
-}
-
-inline void Access(int pos)
-{
-    for (int ch=0;pos;pos=tree[ch=pos].fa) 
-        { splay(pos); RCH=ch; updata(pos); }
-}
-inline void Makeroot(int pos)
-{
-    Access(pos);
-	splay(pos); PutRev(pos);
-}
-inline int Findroot(int pos)
-{
-    Access(pos); splay(pos);
-    while (LCH) { pushdown(pos); pos=LCH; }
-    splay(pos); return pos;
-}
-inline void Split(int x,int y)
-{
-    Makeroot(x);
-	Access(y); splay(y);
-}
-inline int Link(int x,int y)
-{
-    Makeroot(x); 
-    if (Findroot(y)==x) return -1;
-    tree[x].fa=y; return 1;
-}
-inline int Cut(int x,int y)
-{
-    Makeroot(x);
-    if (Findroot(y)==x && tree[y].fa==x && (!tree[y].ch[0]))
-        { tree[y].fa=tree[x].ch[1]=0; updata(x); return 1; }
-    return -1;
-}
+	#define LCH tree[inx].ch[0]
+	#define RCH tree[inx].ch[1]
+	
+	struct Splay_tree
+	{
+		int ch[2],fa,val,rev;
+	}tree[300010];
+	
+	void Upd(int inx)
+	{
+		tree[inx].val=tree[LCH].val^tree[RCH].val^nodev[inx];
+	}
+	void PutRev(int inx)
+	{
+		swap(LCH,RCH);
+		if (LCH) tree[LCH].rev^=1;
+		if (RCH) tree[RCH].rev^=1;
+	}
+	void PushRev(int inx)
+	{
+		if (!tree[inx].rev) return;
+		tree[inx].rev=0; PutRev(inx);
+	}
+	bool NotRoot(int inx)
+	{
+		return (tree[tree[inx].fa].ch[0]==inx) || (tree[tree[inx].fa].ch[1]==inx);
+	}
+	void AllPushdown(int inx)
+	{
+		if (NotRoot(inx)) AllPushdown(tree[inx].fa);
+		PushRev(inx);
+	}
+	
+	void Rotate(int inx)
+	{
+		int f=tree[inx].fa,g=tree[f].fa,dir=(tree[f].ch[1]==inx);
+		
+		if (NotRoot(f)) tree[g].ch[tree[g].ch[1]==f]=inx;
+		if (tree[inx].ch[!dir]) tree[tree[inx].ch[!dir]].fa=f;
+		tree[f].ch[dir]=tree[inx].ch[!dir]; tree[f].fa=inx;
+		tree[inx].fa=g; tree[inx].ch[!dir]=f;
+		
+		Upd(f); Upd(inx);
+	}
+	void Splay(int inx)
+	{
+		AllPushdown(inx);
+		for (;NotRoot(inx);Rotate(inx))
+		{
+			int f=tree[inx].fa,g=tree[f].fa;
+			if (NotRoot(f))
+			{
+				if ( (tree[g].ch[1]==f) == (tree[f].ch[1]==inx) ) Rotate(f);
+				else Rotate(inx);
+			}
+		}
+	}
+	
+	void Access(int inx)
+	{
+		for (int ch=0;inx;inx=tree[inx].fa)
+		{
+			Splay(inx); RCH=ch;
+			ch=inx; Upd(inx);
+		}
+	}
+	void MakeRoot(int inx)
+	{
+		Access(inx); 
+		Splay(inx); PutRev(inx);
+	}
+	int FindRoot(int inx)
+	{
+		Access(inx); Splay(inx); PushRev(inx);
+		while (LCH) { inx=LCH; PushRev(inx); }
+		Splay(inx); return inx;
+	}
+	void Connect(int u,int v)
+	{
+		MakeRoot(u);
+		Access(v); Splay(v);
+	}
+	int Link(int u,int v)
+	{
+		MakeRoot(u);
+		if (FindRoot(v)==u) return -1;
+		tree[u].fa=v; return 1;
+	}
+	int Cut(int u,int v)
+	{
+		MakeRoot(u);
+		if (FindRoot(v)==u && tree[v].fa==u && (!tree[v].ch[0]))
+		{
+			tree[u].ch[1]=tree[v].fa=0;
+			Upd(u); return 1;
+		}
+		return -1;
+	}
+	
+	#undef LCH
+	#undef RCH
+};
 
 int main()
 {
-    readx(n); readx(m);
-    for (int i=1;i<=n;i++) readx(seq[i]);
-    for (int i=1;i<=m;i++)
-    {
-        readx(opt); readx(cachex); readx(cachey);
-        if (opt==0)
-        {
-            Split(cachex,cachey);
-            printf("%d\n",tree[cachey].val);
-        }
-        else if (opt==1) Link(cachex,cachey);
-        else if (opt==2) Cut(cachex,cachey);
-        else { splay(cachex); seq[cachex]=cachey; updata(cachex); }
-    }
-    return 0;
+	readx(n); readx(m);
+	for (int i=1;i<=n;i++) readx(nodev[i]);
+	
+	while (m--)
+	{
+		int opt,u,v;
+		readx(opt); readx(u); readx(v);
+		if (opt==0)
+		{
+			LCT::Connect(u,v);
+			printf("%d\n",LCT::tree[v].val);
+		}
+		if (opt==1)
+		{
+			LCT::Link(u,v);
+		}
+		if (opt==2)
+		{
+			LCT::Cut(u,v);
+		}
+		if (opt==3)
+		{
+			LCT::Splay(u); 
+			nodev[u]=v; LCT::Upd(u);
+		}
+	}
 }
--- qwq.cpp
+++ qwq.cpp
@@ -3,20 +3,18 @@
 #include<cstring>
 #include<string>
 #include<cmath>
-#include<algorithm>
 #include<iterator>
+#include<algorithm>
 #include<cstdlib>
 #include<queue>
 #include<vector>
-#include<map>
 #include<set>
+#include<map>
 using namespace std;
 typedef long long ll;
 typedef ostream_iterator<int> os_iter_int;
 
-// 万年没写 NTT 了
-const ll mod=998244353;
-ll n,a[500010];
+
 
 template<typename inp_t>
 void readx(inp_t& x)
@@ -27,129 +25,11 @@ void readx(inp_t& x)
 	x*=k;
 }
 
-ll fastpow(ll an,ll p)
-{
-	ll ret=1;
-	for (;p;p>>=1,an=an*an%mod) if (p&1) ret=ret*an%mod;
-	return ret;
-}
 
-namespace FFT
-{
-	const ll G=3;
-	const ll iG=fastpow(3,mod-2);
-	
-	ll revf,revt,rev[500010];
-	void BtFl(ll* y,ll len)
-	{
-		if (len!=revf)
-		{
-			revf=len; revt=-1; ll tmp=1;
-			while (tmp<len) { tmp<<=1; revt++; }
-			for (int i=0;i<len;i++) rev[i]=(rev[i>>1]>>1)|((i&1)<<revt);
-		}
-		for (int i=0;i<len;i++) if (i<rev[i]) swap(y[i],y[rev[i]]);
-	}
-	
-	void NTT(ll* y,ll len,ll mode)
-	{
-		BtFl(y,len);
-		for (int i=1;i<len;i<<=1)
-		{
-			const ll wn=fastpow((mode==1?G:iG),(i<<1)/(len-1));
-			for (int j=0;j<len;j+=(i<<1))
-			{
-				ll w=1;
-				for (int k=0;k<i;k++,w=w*wn%mod)
-				{
-					ll t1=y[j+k],t2=y[i+j+k];
-					y[j+k]=(t1+t2)%mod;
-					y[i+j+k]=(t1-t2+mod)%mod;
-				}
-			}
-		}
-	}
-}
-
-ll tmppoly[500010];
-ll PolyInv(ll* A,ll* B,ll* C,ll _n)
-{
-	if (_n==1) { B[0]=fastpow(A[0],mod-2); return; }
-	PolyInv(A,B,C,(_n+1)>>1);
-	
-	ll len=1; while (len<(_n<<1)) len<<=1;
-	for (int i=0;i<len;i++) C[i]=A[i];
-	for (int i=_n;i<len;i++) C[i]=0;
-	
-	FFT::NTT(C,len,1); FFT::NTT(B,len,1);
-	for (int i=0;i<len;i++) B[i]=(2-B[i]*C[i]%mod+mod)%mod*B[i]%mod;
-	FFT::NTT(B,len,-1);
-	
-	for (int i=_n;i<len;i++) B[i]=0;
-}
-
-ll t1[500010],t2[500010],t3[500010],dr;
-ll l1[500010],l2[500010],r1[500010],r2[500010];
-
-void Solve(ll l,ll r)
-{
-	if (l==r)
-	{
-		t1[0]=1; t1[1]=(mod-a[l]%mod)%mod; dr=1;
-		t2[0]=1; return;
-	}
-	ll mid=(l+r)>>1,drl,drr;
-	Solve(l,mid); drl=dr;
-	for (int i=0;i<=drl;i++) l1[i]=t1[i];
-	for (int i=0;i<drl;i++) l2[i]=t2[i];
-	Solve(mid+1,r); drr=dr;
-	for (int i=0;i<=drr;i++) r1[i]=t1[i];
-	for (int i=0;i<drr;i++) r2[i]=t2[i];
-	
-	dr=drr+drl;
-	ll len=1; while (len<=dr) len<<=1;
-	for (int i=drl+1;i<len;i++) l1[i]=0;
-	for (int i=drr+1;i<len;i++) r1[i]=0;
-	
-	FFT::NTT(l1,len,1); FFT::NTT(r1,len,1);
-	for (int i=0;i<len;i++) t1[i]=l1[i]*r1[i]%mod;
-	FFT::NTT(t1,len,-1); FFT::NTT(l1,len,-1); FFT::NTT(r1,len,-1);
-	
-	for (int i=dr+1;i<len;i++) t1[i]=0;
-	
-	len=1; while (len<dr) len<<=1;
-	for (int i=drl;i<len;i++) l2[i]=0;
-	for (int i=drr;i<len;i++) r2[i]=0;
-	
-	FFT::NTT(l1,len,1); FFT::NTT(r1,len,1);
-	FFT::NTT(l2,len,1); FFT::NTT(r2,len,1);
-	for (int i=0;i<len;i++)
-	{
-		t2[i]=l1[i]*r2[i]%mod;
-		t3[i]=r1[i]*l2[i]%mod;
-	}
-	FFT::NTT(t2,len,-1); FFT::NTT(t3,len,-1);
-	
-	for (int i=0;i<len;i++) t2[i]=(t2[i]+t3[i])%mod;
-	for (int i=dr;i<len;i++) t2[i]=0;
-}
-
-ll t2i[500010];
 
 int main()
 {
-	readx(n);
-	for (int i=1;i<=n;i++) readx(a[i]);
-	Solve(1,n);
 	
-	PolyInv(t2,t2i,tmppoly,n-1);
 	
-	int len=1; while (len<n*2) len<<=1;
-	FFT::NTT(t2i,len,1); FFT::NTT(t1,len,1);
-	for (int i=0;i<len;i++) t1[i]=t1[i]*t2i[i]%mod;
-	FFT::NTT(t1,len,-1);
 	
-	ll ans=0;
-	for (int i=1;i<=n;i++) ans^=t1[i];
-	printf("%lld\n",ans);
 }
